63e114bb4b092d2fd1fe96f5269fcee96cfa30ef
Aiden Appleby <aiden.appleby3@gmail.com>
2025-05-19 14:59:19 -0400

    Initial Commit

---
+++ OneTimePaste/OneTimePasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneTimePasteApp.swift
+//  OneTimePaste
+//
+//  Created by Aiden Appleby on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct OneTimePasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OneTimePasteTests/OneTimePasteTests.swift
@@ -0,0 +1,17 @@
+//
+//  OneTimePasteTests.swift
+//  OneTimePasteTests
+//
+//  Created by Aiden Appleby on 5/19/25.
+//
+
+import Testing
+@testable import OneTimePaste
+
+struct OneTimePasteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
